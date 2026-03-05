31cf0300e4e2e6a9152f38c975ca8614110ae15e
germishuysgeraldine-ctrl <germishuysgeraldine@gmail.com>
2026-03-05 17:55:44 +0200

    Update installsteam.sh

--- installsteam.sh
+++ installsteam.sh
@@ -1,2 +1,3 @@
-apt update; apt install software-properties-common; apt-add-repository non-free; sudo dpkg --add-architecture i386; apt update
+apt update; apt install software-properties-common; apt-add-repository non-free; dpkg --add-architecture i386; apt update
+
 apt install steamcmd
